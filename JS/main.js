67fc2d3b58a8319bdd7c9750e49aa23a3a6a82ec
airahmwtii <airarahmawati20@gmail.com>
2025-03-18 16:32:03 +0700

    [FEAT] submit portofolio challenge

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// console.log("Halo duniaa");
+// document.write("Saya senang sekali")
+
+
+let namaAja = "aira"; //camelCase
+console.log(namaAja);
+console.log(typeof(namaAja));
+namaAja = 123456;
+console.log(namaAja);
+console.log(typeof(namaAja));
+
+let isFemale =false; 
+
+
+var nama1
+// const nama2
